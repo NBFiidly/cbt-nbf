9d5f83df2b704892db23a2f9eb132e1affcee77c
FUNIXXX <113288089+NBFiidly@users.noreply.github.com>
2026-09-01 12:07:04 +0700

    Add Course resource routes for teacher role in dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\CourseController;
 use App\Http\Controllers\ProfileController;
+use App\Models\CourseQuestion;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +17,13 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::prefix('dashboard')->name('dashboard.')->group(function () {
+
+        Route::resource('courses', CourseController::class)->middleware('role:teacher');
+
+    });
+
 });
 
 require __DIR__.'/auth.php';
